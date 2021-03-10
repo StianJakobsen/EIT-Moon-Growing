4b18c42f1fb9364f89682199af4ba08c658ec00a
StianJakobsen <stian.mjelde20@hotmail.com>
2021-03-10 14:32:41 +0100

    gltf animation will not show on home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import '@google/model-viewer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const modelViewerStyle = {
+  width: 400,
+  height: 600,
+  margin: 0,
+  position: 'absolute', left: '50%', top: '50%',
+  transform: 'translate(-50%, -50%)',
 }
+
+const App = () => (
+  <div style={modelViewerStyle}>
+    <model-viewer src="../assets/CHAHIN_BOTTLE_OF_SODA-1.gltf" camera-controls></model-viewer>
+  </div>
+);
 
 export default App;
